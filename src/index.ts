ebc8ca9764eb8fb02ebf06865d150ca194c7c9e5
Braulio <braulio.jose.silva@gmail.com>
2024-04-17 00:19:29 +0100

    User model schema done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +21,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log("Server running on http://localhost:8080")
 })
+
+const MONGO_DB_URL = "mongodb+srv://bjhs:emI4DsDiXk4ztJtH@cluster0.pdohotc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_DB_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
