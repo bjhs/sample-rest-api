d6efec35e066ef04cd4c83fc6b316bb114ba1e1a
Braulio <braulio.jose.silva@gmail.com>
2024-04-17 02:26:00 +0100

    Login controller, concentrate route on single file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import authentication from './authentication';
-import login from './login';
 const router = express.Router();
 
 export default (): express.Router => {
     authentication(router);
-    login(router);
     return router;
 }
